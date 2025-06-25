c9844b1f1d1889cbf84ce796f8c3e6f5b0b243de
parani <paranitharanparani13@gmail.com>
2025-06-25 10:16:53 +0530

    Update ESLint configuration to include recommended plugins for TypeScript and React

--- myapp/eslint.config.mjs
+++ myapp/eslint.config.mjs
@@ -10,8 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  // ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"),
-];
-
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+]
 export default eslintConfig;
